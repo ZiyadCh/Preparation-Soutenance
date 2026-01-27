31a2d5fdcaa9f49ed80ed15af4acc319f16925e1
ZiyadCh <chetoziyad@gmail.com>
2026-01-27 20:19:17 +0100

    sqlfile

---
+++ sql/sql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Produits (
+  id integer auto_increment,
+  nom varchar(10),
+  prix float,
+  stock integer
+);
+INSERT INTO Produits (nom,prix,stock) VALUES ('tv',2000,100);
+
+UPDATE Produits
+  SET prix = prix * 0.10
+  WHERE id = 2;
+
+  DELETE FROM Produits
+    WHERE stock = 0;
+
